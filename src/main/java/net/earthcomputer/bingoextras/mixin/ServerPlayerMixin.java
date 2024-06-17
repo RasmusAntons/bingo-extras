30b46d0ee973ff52ecf0b9884f47b09bae6123d5
rasmus <mail@rasmusantons.de>
2024-06-17 22:03:26 +0200

    spawn players in the lobby dimension

--- src/main/java/net/earthcomputer/bingoextras/mixin/ServerPlayerMixin.java
+++ src/main/java/net/earthcomputer/bingoextras/mixin/ServerPlayerMixin.java
@@ -3,6 +3,9 @@ package net.earthcomputer.bingoextras.mixin;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.mojang.authlib.GameProfile;
+import io.github.gaming32.bingo.Bingo;
+import net.earthcomputer.bingoextras.Configs;
+import net.earthcomputer.bingoextras.FantasyLobby;
 import net.earthcomputer.bingoextras.ext.PlayerTeamExt;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.GlobalPos;
@@ -16,9 +19,7 @@ import net.minecraft.world.level.portal.DimensionTransition;
 import net.minecraft.world.scores.PlayerTeam;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerPlayer.class)
 public abstract class ServerPlayerMixin extends Player {
@@ -54,6 +55,11 @@ public abstract class ServerPlayerMixin extends Player {
 
     @ModifyExpressionValue(method = "findRespawnPositionAndUseSpawnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;overworld()Lnet/minecraft/server/level/ServerLevel;"))
     private ServerLevel respawnTeamInDimension(ServerLevel original) {
+        if (Configs.createFantasyLobby && Bingo.activeGame == null) {
+            FantasyLobby lobby = FantasyLobby.INSTANCE;
+            return lobby.getWorld();
+        }
+
         PlayerTeam team = this.getTeam();
         if (team != null) {
             GlobalPos respawnPos = PlayerTeamExt.getTeamSpawnPos(team);
