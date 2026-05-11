da94cde1068048082cc84e6c63b6b6adaf9dd931
rasmus <mail@rasmusantons.de>
2026-05-11 12:58:01 +0200

    fix nether respawns

--- src/main/java/net/earthcomputer/bingoextras/mixin/PlayerSpawnFinderMixin.java
+++ src/main/java/net/earthcomputer/bingoextras/mixin/PlayerSpawnFinderMixin.java
@@ -2,10 +2,15 @@ package net.earthcomputer.bingoextras.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
+import net.earthcomputer.bingoextras.command.BingoSpreadPlayersCommand;
 import net.earthcomputer.bingoextras.ext.fantasy.ServerLevelExt_Fantasy;
+import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.PlayerSpawnFinder;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.GameType;
+import net.minecraft.world.phys.Vec3;
+import org.joml.Vector2d;
+import org.joml.Vector3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -18,5 +23,19 @@ public class PlayerSpawnFinderMixin {
             return GameType.SURVIVAL;
         }
         return defaultGameType;
+    }
+
+    @ModifyExpressionValue(method = "findSpawn", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/PlayerSpawnFinder;fixupSpawnHeight(Lnet/minecraft/world/level/CollisionGetter;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/phys/Vec3;"))
+    private static Vec3 overwriteNetherSpawn(
+            Vec3 original,
+            @Local(name = "level", argsOnly = true) ServerLevel level,
+            @Local(name = "spawnSuggestion", argsOnly = true) BlockPos spawnSuggestion
+    ) {
+        ServerLevel parentLevel = ((ServerLevelExt_Fantasy) level).bingoExtras$getParentLevel();
+        if (parentLevel != null && parentLevel.dimension() == ServerLevel.NETHER) {
+            Vector3d safeLocation = BingoSpreadPlayersCommand.adjustToSafeLocation(level, new Vector2d(spawnSuggestion.getX(), spawnSuggestion.getY()));
+            return new Vec3(safeLocation.x, safeLocation.y, safeLocation.z);
+        }
+        return original;
     }
 }
